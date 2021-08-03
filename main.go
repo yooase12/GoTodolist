e88cbe5437f2898891b9b5bb92b66bae3052e909
박경현 <tooli38@gmail.com>
2021-08-03 16:25:31 +0900

    Add test12

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func dbInit() (*gorm.DB, error) {
-	user := os.Getenv("DBUSER")
+	user := os.Getenv("DBUSER")dd
 	pass := os.Getenv("DBPASS")
 	dsn := fmt.Sprintf("%s:%s%s", user, pass, "@tcp(127.0.0.1:3306)/todolist?charset=utf8&parseTime=True&loc=Local")
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
